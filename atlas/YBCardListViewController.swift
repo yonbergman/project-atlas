78c03a076e70a1f2998b68a104ae60793dbac4d6
yon <Lighthawky@gmail.com>
2016-01-09 18:56:21 +0200

    bump version

--- atlas/YBCardListViewController.swift
+++ atlas/YBCardListViewController.swift
@@ -29,10 +29,6 @@ class YBCardListViewController: UITableViewController, UISearchDisplayDelegate, 
 
   override func awakeFromNib() {
     super.awakeFromNib()
-    if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-      self.clearsSelectionOnViewWillAppear = false
-      self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
-    }
     self.clearsSelectionOnViewWillAppear = false
   }
 
